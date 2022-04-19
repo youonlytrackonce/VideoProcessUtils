c6d71efefdf6c4260075b17dc1de407d856207e5
youonlytrackonce <f_emre87@hotmail.com>
2022-04-19 22:00:52 +0300

    Paper album generated

--- imageEditor.py
+++ imageEditor.py
@@ -8,22 +8,80 @@ import numpy as np
 from operator import itemgetter
 
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from glob import glob
+import requests
+from cairosvg import svg2png
+
+def rect_with_rounded_corners(image, r, t, c):
+    """
+    :param image: image as NumPy array
+    :param r: radius of rounded corners
+    :param t: thickness of border
+    :param c: color of border
+    :return: new image as NumPy array with rounded corners
+    """
+
+    c += (255, )
+
+    h, w = image.shape[:2]
+
+    # Create new image (three-channel hardcoded here...)
+    new_image = np.ones((h+2*t, w+2*t, 4), np.uint8) * 255
+    new_image[:, :, 3] = 0
+
+    # Draw four rounded corners
+    new_image = cv2.ellipse(new_image, (int(r+t/2), int(r+t/2)), (r, r), 180, 0, 90, c, t)
+    new_image = cv2.ellipse(new_image, (int(w-r+3*t/2-1), int(r+t/2)), (r, r), 270, 0, 90, c, t)
+    new_image = cv2.ellipse(new_image, (int(r+t/2), int(h-r+3*t/2-1)), (r, r), 90, 0, 90, c, t)
+    new_image = cv2.ellipse(new_image, (int(w-r+3*t/2-1), int(h-r+3*t/2-1)), (r, r), 0, 0, 90, c, t)
+
+    # Draw four edges
+    new_image = cv2.line(new_image, (int(r+t/2), int(t/2)), (int(w-r+3*t/2-1), int(t/2)), c, t)
+    new_image = cv2.line(new_image, (int(t/2), int(r+t/2)), (int(t/2), int(h-r+3*t/2)), c, t)
+    new_image = cv2.line(new_image, (int(r+t/2), int(h+3*t/2)), (int(w-r+3*t/2-1), int(h+3*t/2)), c, t)
+    new_image = cv2.line(new_image, (int(w+3*t/2), int(r+t/2)), (int(w+3*t/2), int(h-r+3*t/2)), c, t)
+
+    # Generate masks for proper blending
+    mask = new_image[:, :, 3].copy()
+    mask = cv2.floodFill(mask, None, (int(w/2+t), int(h/2+t)), 128)[1]
+    mask[mask != 128] = 0
+    mask[mask == 128] = 1
+    mask = np.stack((mask, mask, mask), axis=2)
+
+    # Blend images
+    temp = np.zeros_like(new_image[:, :, :3])
+    temp[(t-1):(h+t-1), (t-1):(w+t-1)] = image.copy()
+    new_image[:, :, :3] = new_image[:, :, :3] * (1 - mask) + temp * mask
+
+    # Set proper alpha channel in new image
+    temp = new_image[:, :, 3].copy()
+    new_image[:, :, 3] = cv2.floodFill(temp, None, (int(w/2+t), int(h/2+t)), 255)[1]
+
+    return new_image
 
 
-images_dir = '/home/fatih/phd/VideoProcessUtils/'
-def plot_album(album_name):
-    fig, axes = plt.subplots(nrows=1, ncols=3)
-    # this assumes the images are in images_dir/album_name/<name>.jpg
-    image_paths = glob(images_dir + album_name + '/*.jpg')
-    for imp, ax in zip(image_paths, axes.ravel()):
-        img = mpimg.imread(imp)
-        ax.imshow(img)
-        ax.axis('off')
-    fig.tight_layout()
-    plt.savefig('hists.jpg', dpi=300)
-    fig.show()
+"""
+images_dir = '/mnt/disk1/trackeveryseason/album/'
+fig, axes = plt.subplots(nrows=1, ncols=8,figsize=(19.2, 10.8))
+seqs = os.listdir(images_dir)
+for ix, seq in enumerate(seqs):
+    img = os.listdir(images_dir + seq)
+    new_img = cv2.imread(images_dir + seq + '/' + img[0])
+    # cv2.imshow('{}'.format(seq), new_img)
+    new_img = rect_with_rounded_corners(new_img, 100, 20, (255, 255, 255))
+    new_img = cv2.cvtColor(new_img, cv2.COLOR_BGR2RGB)
+    axes[ix].imshow(new_img)
+    axes[ix].axis('off')
+plt.subplots_adjust(left=0.1,
+                    bottom=0.1,
+                    right=0.9,
+                    top=0.9,
+                    wspace=0.02,
+                    hspace=0.1)
+plt.savefig('album.png', dpi=100)
+fig.show()
+plt.axis('off')
+"""
+new_img = cv2.imread('album.png')
+crop_img = new_img[480:600, 190:1730]
+cv2.imwrite('album_crop.png', crop_img)
 
-
-plot_album('hists')
